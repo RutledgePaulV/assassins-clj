a57693797fa9bc56833ac0cc09aa71a79c979f84
paul.rutledge <paul.rutledge@vodori.com>
2016-09-25 13:14:17 -0500

    start of pages

--- src/cljs/assassins/core.cljs
+++ src/cljs/assassins/core.cljs
@@ -5,7 +5,26 @@
 
 (defonce app-state (atom {:text "Hello Chestnut!"}))
 
-(defn greeting []
-  [:h1 (:text @app-state)])
 
-(reagent/render [greeting] (js/document.getElementById "app"))
+(defn nav []
+  [:nav.ui.menu
+   [:a.header.item {:href "/"} "Home"]
+   [:a.item {:href "/games"} "Games"]
+   [:a.item {:href "/geek"} "Geek"]
+   [:a.item {:href "/about"} "About"]
+   [:a.item.right {:href "/profile"} "Profile"]])
+
+(defn main []
+  [:main.ui
+   [:div {:style {:margin-top "30px"}}
+    [:div.ui.two.column.centered.grid
+     [:div.ui.row.segment.basic
+      [:h2.ui.header.center "You ready killa?"]]]]])
+
+(defn index []
+  [:div
+   [:div.header [nav]]
+   [:div.body [main]]
+   [:div.footer]])
+
+(reagent/render [index] (js/document.getElementById "app"))
